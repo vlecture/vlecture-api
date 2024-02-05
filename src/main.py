d3376a5cd72ddf2e8a3d2399227ddfb6dc05ea31
jeremyalv <jeremyalvax@gmail.com>
2024-02-05 17:00:11 +0700

    dpl: cors

--- src/main.py
+++ src/main.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI
 from src.routes.routes import router
 from src.utils.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # For testing 
 from fastapi.testclient import TestClient
 
 app = FastAPI()
 app.include_router(router)
+
+# CORS
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "api.vlecture.com",
+    "vlecture-api-production.up.railway.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
