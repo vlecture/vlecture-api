7c12b022c485241f2fc6d6cb2eafbc4d0a7f51c0
Daffa <daffamfzn@gmail.com>
2024-02-24 12:48:22 +0700

    feat: added a new user model

--- src/models/user/user.py
+++ src/models/user/user.py
@@ -1,22 +1,55 @@
-from datetime import datetime
-from typing_extensions import Optional
-from src.utils.database import Base
-from pydantic import BaseModel
-from sqlalchemy import Column, Integer, String, TIMESTAMP, text
-
-
-class UserBase(BaseModel):
-    id: Optional[int]
-    username: str
-    created_at: Optional[datetime]
-
-    class Config:
-        orm_mode = True
+import bcrypt
+import jwt
+from src.utils.db import Base
+from src.utils.settings import SECRET
+from sqlalchemy import (
+    Boolean,
+    Column,
+    Integer,
+    LargeBinary,
+    PrimaryKeyConstraint,
+    String,
+    UniqueConstraint,
+)
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    username = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"))
+    id = Column(Integer, nullable=False, primary_key=True)
+    email = Column(String(225), nullable=False, unique=True)
+    first_name = Column(String(225))
+    last_name = Column(String(225))
+    hashed_password = Column(LargeBinary, nullable=False)
+    is_active = Column(Boolean, default=False)
+
+    UniqueConstraint("email", name="uq_user_email")
+    PrimaryKeyConstraint("id", name="pk_user_id")
+
+    def __repr__(self):
+        """Returns string representation of model instance"""
+        return "<User {first_name}>".format(first_name=self.first_name)
+
+    @staticmethod
+    def hash_password(password: bytes) -> bytes:
+        """Transforms password from it's raw textual form to
+        cryptographic hashes
+        """
+        return bcrypt.hashpw(password, bcrypt.gensalt())
+
+    def validate_password(self, password: str) -> bool:
+        """Confirms password validity"""
+        return bcrypt.checkpw(password.encode(), self.hashed_password)
+
+    def generate_token(self) -> dict:
+        """Generate access token for user"""
+        return {
+            "access_token": jwt.encode(
+                {
+                    "first_name": self.first_name,
+                    "last_name": self.last_name,
+                    "email": self.email,
+                },
+                SECRET,
+            )
+        }
