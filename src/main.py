61fbd376b02521ffb478a75a8d3dd5ea0014ac1f
Daffa <daffamfzn@gmail.com>
2024-02-25 22:01:10 +0700

    feat: updated login and register logic

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from typing import Dict
 
-from starlette.status import HTTP_400_BAD_REQUEST
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_500_INTERNAL_SERVER_ERROR
 
 from src.utils.db import Base, engine, get_db
 from src.schemas.users import CreateUserSchema, UserLoginSchema
@@ -45,7 +45,7 @@ def hi():
 @app.post("/signup", tags=["auth"])
 def signup(payload: CreateUserSchema = Body(), session: Session = Depends(get_db)):
     """Processes request to register user account."""
-    existing_user = None
+    user = None
     if (
         len(payload.email) == 0
         or len(payload.first_name) == 0
@@ -57,11 +57,11 @@ def signup(payload: CreateUserSchema = Body(), session: Session = Depends(get_db
             detail="All required fields must be filled!",
         )
     try:
-        existing_user = get_user(session=session, email=payload.email)
+        user = get_user(session=session, email=payload.email)
     except Exception:
         payload.hashed_password = User.hash_password(payload.hashed_password)
         return create_user(session, user=payload)
-    if existing_user:
+    if user:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT, detail="User already exists!"
         )
@@ -81,17 +81,20 @@ def login(payload: UserLoginSchema = Body(), session: Session = Depends(get_db))
 
     - password
     """
+    user = None
     try:
-        user: User = get_user(session=session, email=payload.email)
+        user = get_user(session=session, email=payload.email)
     except Exception:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid user credentials"
-        )
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="User not found!"
+            )
+    else:
+        is_validated: bool = user.validate_password(payload.password)
+        if not is_validated:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid user credentials",
+            )
 
-    is_validated: bool = user.validate_password(payload.password)
-    if not is_validated:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid user credentials"
-        )
-
-    return user.generate_token()
+        return user.generate_token()
