667d5258fa86053d5d375b09239425c7348d43ad
Daffa <daffamfzn@gmail.com>
2024-02-25 19:12:14 +0700

    feat: added new http exception when any required fields are empty

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from typing import Dict
 
+from starlette.status import HTTP_400_BAD_REQUEST
+
 from src.utils.db import Base, engine, get_db
 from src.schemas.users import CreateUserSchema, UserLoginSchema
 from src.models.users import User
@@ -43,7 +45,22 @@ def hi():
 @app.post("/signup", tags=["auth"])
 def signup(payload: CreateUserSchema = Body(), session: Session = Depends(get_db)):
     """Processes request to register user account."""
-    existing_user: User = get_user(session=session, email=payload.email)
+    existing_user = None
+    if (
+        len(payload.email) == 0
+        or len(payload.first_name) == 0
+        or (payload.last_name) == 0
+        or (payload.hashed_password) == 0
+    ):
+        raise HTTPException(
+            status_code=HTTP_400_BAD_REQUEST,
+            detail="All required fields must be filled!",
+        )
+    try:
+        existing_user = get_user(session=session, email=payload.email)
+    except Exception:
+        payload.hashed_password = User.hash_password(payload.hashed_password)
+        return create_user(session, user=payload)
     if existing_user:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT, detail="User already exists!"
