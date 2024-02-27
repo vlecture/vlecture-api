6670a1c08383e58b4549c06ec9e184330065d8a1
jeremyalv <jeremyalvax@gmail.com>
2024-02-27 23:01:18 +0700

    feat: add coverage requirements, add Transcription Schema

---
+++ src/schemas/transcription.py
@@ -0,0 +1,19 @@
+from datetime import date, datetime, time, timedelta
+from pydantic import UUID4, BaseModel, Field, List, Optional
+
+class TranscriptionChunks(BaseModel):
+  id: UUID4
+  transcription_id: UUID4
+  duration: float # how many seconds in chunk
+  content: Optional[str]
+  created_at: datetime
+  updated_at: datetime
+  is_edited: bool
+
+class TranscriptionSchema(BaseModel):
+  id: UUID4
+  title: Optional[str]
+  tags: Optional[List[int]]
+  chunks: Optional[List[TranscriptionChunks]]
+  duration: float # total seconds of transcription
+  created_at: datetime
