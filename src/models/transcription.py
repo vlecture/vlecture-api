c8b58b285f3324c67a056b213886d8d6ee797d27
jeremyalv <jeremyalvax@gmail.com>
2024-03-01 11:59:15 +0700

    feat: updated ORM model definition

--- src/models/transcription.py
+++ src/models/transcription.py
@@ -1,20 +1,30 @@
 import uuid
 from datetime import datetime, timedelta, timezone
+from __future__ import annotations
+from typing import List
+
 from src.utils.db import Base
+from src.models.base import DBBase
+from src.models.users import User
+from src.schemas.transcription import TranscriptionSchema, TranscriptionChunksSchema
 from src.utils.settings import REFRESH_TOKEN_SECRET, ACCESS_TOKEN_SECRET
+
+
 from sqlalchemy import (
-    Boolean,
     Column,
     UUID,
-    ARRAY,
-    LargeBinary,
-    PrimaryKeyConstraint,
+    Integer,
     String,
     Float,
+    Boolean,
+    LargeBinary,
+    PrimaryKeyConstraint,
     DateTime,
     UniqueConstraint,
     ForeignKey,
 )
+
+from sqlalchemy.dialects.postgresql import ARRAY
 
 from sqlalchemy.sql import func
 
@@ -25,65 +35,59 @@ from sqlalchemy.orm import (
   relationship,
 )
 
-from __future__ import annotations
-from typing import List
-
-
-class TranscriptionChunk(Base):
-  __tablename__ = "transcription_chunk"
-
-  id: Mapped[UUID] = mapped_column(
-    "id",
-    UUID(as_uuid=True),
-    nullable = False,
-    primary_key = True,
-    default = uuid.uuid4
-  )
-
-  transcription_id: Mapped[UUID] = mapped_column(ForeignKey("transcription.id"))
-
-  transcription_parent: Mapped["Transcription"] = relationship("Transcription", back_populates="chunks")
-
-  duration: Mapped[float] = mapped_column(Float, min=0, nullable=False, default=0) 
-
-  created_at: Mapped[datetime] = mapped_column(
-    DateTime(timezone=True), server_default=func.now()
-  )
-
-  updated_at: Mapped[datetime] = mapped_column(
-    DateTime(timezone=True), server_default=func.now()
-  )
-
-  is_edited: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
-
-  
-  
-
 class Transcription(Base):
   __tablename__ = "transcription"
+
+  ## Foreign Relationships
+  # Foreign key to User
+  owner_id = Column(UUID, ForeignKey(User.id), nullable=False)
   
-  id: Mapped[UUID] = mapped_column(
-    "id",
-    UUID(as_uuid=True),
-    nullable = False,
-    primary_key = True,
-    default = uuid.uuid4
-  )
+  # TODO in case User gets a `transcriptions` field
+  # owner = relationship("User", back_populates="transcriptions")
+  
+  title = Column(String(255), nullable=False, unique=False)
+  
+  tags = Column(ARRAY(String), nullable=True, unique=False)
 
-  title: Mapped[String] = mapped_column(String(255), nullable=False, unique=False)
+  chunks = relationship("TranscriptionChunk", back_populates="transcription_parent")
 
-  tags: Mapped[ARRAY] = mapped_column(ARRAY(String), nullable=True)
+  duration = Column(Float(precision=1), default=0, nullable=False)
 
-  chunk: Mapped[List["TranscriptionChunk"]] = relationship("TranscriptionChunk", back_populates="transcription_parent")
+  def __to_model(self) -> TranscriptionSchema:
+    """ Converts DB ORM object to Pydantic Model ('schema') """
+    return TranscriptionSchema.model_validate(self)
+  
+  # TODO DEBUG
+  @classmethod
+  def get_by_id(cls, uuid: UUID) -> TranscriptionSchema:
+    base = super().get_by_id(uuid)
+    return base.__to_model() if base else None
 
-  duration: Mapped[Float] = mapped_column(Float, nullable=False, default = 0) 
 
-  created_at: Mapped[datetime] = mapped_column(
-    DateTime(timezone=True), server_default=func.now()
-  )
+class TranscriptionChunk(Base, DBBase):
+  __tablename__ = "transcription_chunk"
 
-  updated_at: Mapped[datetime] = mapped_column(
-    DateTime(timezone=True), server_default=func.now()
-  )
+  # Extra fields
+  duration = Column(Float(precision=1), default=0, nullable=False)
+
+  is_edited = Column(Boolean, default=False)
+
+  ## Foreign Relationships
+  # Foreign key to Transcription
+  transcription_id = Column(UUID, ForeignKey(Transcription.id), nullable=False)
+
+  # One to many relationship with Transcription
+  transcription_parent = relationship("Transcription", back_populates="chunks")
+
+  def __to_model(self) -> TranscriptionChunksSchema:
+    """ Converts DB ORM object to Pydantic Model ('schema') """
+    return TranscriptionChunksSchema.model_validate(self)
+  
+  @classmethod
+  def get_by_id(cls, uuid: UUID) -> TranscriptionChunksSchema:
+    base = super().get_by_id(uuid)
+    return base.__to_model() if base else None
+
+  
 
   
