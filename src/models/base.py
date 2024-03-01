8f72797abc4ec5dbd8e6c3c3cfda25493fd0107d
jeremyalv <jeremyalvax@gmail.com>
2024-03-01 10:58:20 +0700

    feat: created base model and swapped with previous base schema

--- src/models/base.py
+++ src/models/base.py
@@ -1,28 +1,31 @@
-import http
 from datetime import datetime
-from typing import Optional, Any, Union
-from uuid import UUID
 
-from pydantic.main import BaseModel
-class GenericResponseModel(BaseModel):
+import uuid as uuid
+from pytz import timezone
+from typing import Any
+
+from sqlalchemy import Column, TIMESTAMP, Boolean, Integer, create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.dialects.postgresql import UUID
+
+from src.utils.db import get_db
+
+
+UTC = timezone("UTC")
+def time_now():
+    return datetime.now(UTC)
+
+class DBBase:
   """
-  A generic response model for all API responses
+  Base Class for all DB models
   """
 
-  status_code: Optional[Union[http.HTTPStatus, int]] = None
-  error: Optional[str]
-  message: Optional[str]
-  data: Any
+  id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
+  created_at = Column(TIMESTAMP(timezone=True), default=time_now, nullable=False)
+  updated_at = Column(TIMESTAMP(timezone=True), default=time_now, onupdate=time_now, nullable=False)
+  is_deleted = Column(Boolean, default=False)
 
-class DBBaseModel(BaseModel):
-  """
-  Base model for all the models to be stored in the database
-  """
-
-  id: UUID
-  created_at: datetime
-  updated_at: Optional[datetime]
-  is_deleted: datetime
-
-  class Config:
-    orm_mode = True
+  @classmethod
+  def get_by_uuid(cls, uuid: UUID) -> Any:
+     db: Session = get_db()
+     return db.query(cls).filter(cls.id == uuid, cls.is_deleted._is_(False)).first()
