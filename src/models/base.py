c3e4f6a9126285bc54d7505accbfa5754874898e
jeremyalv <jeremyalvax@gmail.com>
2024-03-01 10:39:10 +0700

    feat: init modules and created project base models

---
+++ src/models/base.py
@@ -0,0 +1,28 @@
+import http
+from datetime import datetime
+from typing import Optional, Any, Union
+from uuid import UUID
+
+from pydantic.main import BaseModel
+class GenericResponseModel(BaseModel):
+  """
+  A generic response model for all API responses
+  """
+
+  status_code: Optional[Union[http.HTTPStatus, int]] = None
+  error: Optional[str]
+  message: Optional[str]
+  data: Any
+
+class DBBaseModel(BaseModel):
+  """
+  Base model for all the models to be stored in the database
+  """
+
+  id: UUID
+  created_at: datetime
+  updated_at: Optional[datetime]
+  is_deleted: datetime
+
+  class Config:
+    orm_mode = True
