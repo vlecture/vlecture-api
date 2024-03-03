118d40512611d14f28286e567e01eefd04f72e05
Daffa <daffamfzn@gmail.com>
2024-03-03 18:16:00 +0700

    feat: added positive test cases for login and register

--- tests/main.py
+++ tests/main.py
@@ -8,3 +8,33 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
+
+
+## Authentication Tests ##
+
+# Positive Cases
+
+
+def test_register_positive():
+    response = client.post(
+        "/register",
+        json={
+            "email": "positive@example.com",
+            "first_name": "Positive",
+            "middle_name": "Test",
+            "last_name": "Case",
+            "hashed_password": "positivepassword",
+        },
+    )
+    assert response.status_code == 200
+    assert response.json()["email"] == "positive@example.com"
+
+
+def test_login_positive():
+    # Assuming a user "positive@example.com" already exists from the register test
+    response = client.post(
+        "/login", json={"email": "positive@example.com", "password": "positivepassword"}
+    )
+    assert response.status_code == 200
+    assert "access_token" in response.json()
+    assert "refresh_token" in response.json()
