304e7d64595538460f4c37ec867d437a2b65a325
Jeremy <42536973+jeremyalv@users.noreply.github.com>
2024-03-11 21:15:23 +0700

    hotfix: print which db vars are None

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -15,10 +15,12 @@ db_name = os.getenv("POSTGRES_DB")
 username = os.getenv("POSTGRES_USER")
 password = os.getenv("POSTGRES_PASSWORD")
 
-if None in [host, port, db_name, username, password]:
-    raise ValueError(
-        "One or more environment variables for the database configuration are not set"
-    )
+# Check if any DB env vars are None
+env_vars = ['host', 'port', 'db_name', 'username', 'password']
+values = [os.environ.get(var) for var in env_vars]
+none_values = [var for var, val in zip(env_vars, values) if val is None]
+if none_values:
+    raise ValueError("One or more environment variables for the database configuration are not set: {}".format(', '.join(none_values)))
 
 # Database url configuration
 DATABASE_URL = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{db_name}"
@@ -31,3 +33,14 @@ AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 SENTRY_DSN = os.getenv("SENTRY_DSN")
+
+MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+MAIL_FROM = os.getenv("MAIL_FROM")
+MAIL_PORT = os.getenv("MAIL_PORT")
+MAIL_SERVER = os.getenv("MAIL_SERVER")
+MAIL_FROM_NAME = os.getenv("MAIL_FROM_NAME")
+MAIL_STARTTLS = os.getenv("MAIL_STARTTLS")
+MAIL_SSL_TLS = os.getenv("MAIL_SSL_TLS")
+USE_CREDENTIALS = os.getenv("USE_CREDENTIALS")
+VALIDATE_CERTS = os.getenv("VALIDATE_CERTS")
