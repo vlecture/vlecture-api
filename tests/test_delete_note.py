8ee4d552f00efadf99e7d8a5cf280b6de72fd743
Annava Wisha Sikoko <annava.wisha@ui.ac.id>
2024-03-24 20:32:27 +0700

    feat: testing delete note

---
+++ tests/test_delete_note.py
@@ -0,0 +1,26 @@
+from tests.utils.test_db import client, test_db
+
+
+def test_delete_note_positive():
+    '''Delete your own existing note'''
+    response = client.delete("/notes/{note_id}", headers={"Authorization": "Bearer YOUR_ACCESS_TOKEN"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Note deleted successfully"}
+
+def test_delete_note_invalid_id():
+    '''Delete your own unexisting note'''
+    response = client.delete("/notes/999", headers={"Authorization": "Bearer YOUR_ACCESS_TOKEN"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Note not found"}
+
+def test_delete_note_missing_id():
+    '''Delete your note without any note_id'''
+    response = client.delete("/notes/", headers={"Authorization": "Bearer YOUR_ACCESS_TOKEN"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Not Found"}
+
+def test_delete_note_negative():
+    '''Delete someone else's note'''
+    response = client.delete("/notes/{note_id}", headers={"Authorization": "Bearer YOUR_ACCESS_TOKEN"})
+    assert response.status_code == 403
+    assert response.json() == {"detail": "You are not allowed to delete this note"}
