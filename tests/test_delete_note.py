845cc0fa29cfb8cea294f1395e8b4cbfb5d4776b
Annava Wisha Sikoko <annava.wisha@ui.ac.id>
2024-03-24 20:42:15 +0700

    feat: added test_db

--- tests/test_delete_note.py
+++ tests/test_delete_note.py
@@ -1,25 +1,25 @@
 from tests.utils.test_db import client, test_db
 
 
-def test_delete_note_positive():
+def test_delete_note_positive(test_db):
     '''Delete your own existing note'''
     response = client.delete("/notes/{note_id}", headers={"Authorization": "Bearer YOUR_ACCESS_TOKEN"})
     assert response.status_code == 200
     assert response.json() == {"message": "Note deleted successfully"}
 
-def test_delete_note_invalid_id():
+def test_delete_note_invalid_id(test_db):
     '''Delete your own unexisting note'''
     response = client.delete("/notes/999", headers={"Authorization": "Bearer YOUR_ACCESS_TOKEN"})
     assert response.status_code == 404
     assert response.json() == {"detail": "Note not found"}
 
-def test_delete_note_missing_id():
+def test_delete_note_missing_id(test_db):
     '''Delete your note without any note_id'''
     response = client.delete("/notes/", headers={"Authorization": "Bearer YOUR_ACCESS_TOKEN"})
     assert response.status_code == 404
     assert response.json() == {"detail": "Not Found"}
 
-def test_delete_note_negative():
+def test_delete_note_negative(test_db):
     '''Delete someone else's note'''
     response = client.delete("/notes/{note_id}", headers={"Authorization": "Bearer YOUR_ACCESS_TOKEN"})
     assert response.status_code == 403
