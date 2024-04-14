69e0216b48d47b7bdf85f3497381e75ae7a1fa8d
Daffa <daffamfzn@gmail.com>
2024-04-14 19:20:29 +0700

    feat: added function to check if settings.py currently being run on github actions or not to determine take env from secrets

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -1,60 +1,67 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(".env", override=True)
+def get_env_variable(name):
+  value = os.getenv(name)
+  if value is None and os.environ.get('GITHUB_ACTIONS', False):
+    value = os.environ.get(f'secrets.{name}')
+  return value
+
+if not os.environ.get('GITHUB_ACTIONS', False):
+  load_dotenv(".env", override=True)
 
 ENV_TYPE = os.getenv("ENV")
 
 print(f"ENV: {ENV_TYPE}")
 
 # POSTGRES
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = get_env_variable("POSTGRES_HOST")
+POSTGRES_PORT = get_env_variable("POSTGRES_PORT")
+POSTGRES_DB = get_env_variable("POSTGRES_DB")
+POSTGRES_USER = get_env_variable("POSTGRES_USER")
+POSTGRES_PASSWORD = get_env_variable("POSTGRES_PASSWORD")
 
 # TESTDB
-TESTDB_URL = os.getenv("TESTDB_URL")
+TESTDB_URL = get_env_variable("TESTDB_URL")
 
 # MONGO DB
-MONGODB_URL = os.getenv("MONGODB_URL")
+MONGODB_URL = get_env_variable("MONGODB_URL")
 
-MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME")
-MONGODB_COLLECTION_NAME = os.getenv("MONGODB_COLLECTION_NAME")
+MONGODB_DB_NAME = get_env_variable("MONGODB_DB_NAME")
+MONGODB_COLLECTION_NAME = get_env_variable("MONGODB_COLLECTION_NAME")
 
-MONGODB_URL_RW = os.getenv("MONGODB_URL_RW")
-MONGODB_URL_MAJORITY = os.getenv("MONGODB_URL_MAJORITY")
-MONGODB_URL_CLUSTER = os.getenv("MONGODB_URL_CLUSTER")
+MONGODB_URL_RW = get_env_variable("MONGODB_URL_RW")
+MONGODB_URL_MAJORITY = get_env_variable("MONGODB_URL_MAJORITY")
+MONGODB_URL_CLUSTER = get_env_variable("MONGODB_URL_CLUSTER")
 
-REFRESH_TOKEN_SECRET = os.getenv("REFRESH_TOKEN_SECRET")
-ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+REFRESH_TOKEN_SECRET = get_env_variable("REFRESH_TOKEN_SECRET")
+ACCESS_TOKEN_SECRET = get_env_variable("ACCESS_TOKEN_SECRET")
 
-AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_BUCKET_NAME = get_env_variable("AWS_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")
 
-SENTRY_DSN = os.getenv("SENTRY_DSN")
+SENTRY_DSN = get_env_variable("SENTRY_DSN")
 
-MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-MAIL_FROM = os.getenv("MAIL_FROM")
-MAIL_PORT = os.getenv("MAIL_PORT")
-MAIL_SERVER = os.getenv("MAIL_SERVER")
-MAIL_FROM_NAME = os.getenv("MAIL_FROM_NAME")
-MAIL_STARTTLS = os.getenv("MAIL_STARTTLS")
-MAIL_SSL_TLS = os.getenv("MAIL_SSL_TLS")
-USE_CREDENTIALS = os.getenv("USE_CREDENTIALS")
-VALIDATE_CERTS = os.getenv("VALIDATE_CERTS")
+MAIL_USERNAME = get_env_variable("MAIL_USERNAME")
+MAIL_PASSWORD = get_env_variable("MAIL_PASSWORD")
+MAIL_FROM = get_env_variable("MAIL_FROM")
+MAIL_PORT = get_env_variable("MAIL_PORT")
+MAIL_SERVER = get_env_variable("MAIL_SERVER")
+MAIL_FROM_NAME = get_env_variable("MAIL_FROM_NAME")
+MAIL_STARTTLS = get_env_variable("MAIL_STARTTLS")
+MAIL_SSL_TLS = get_env_variable("MAIL_SSL_TLS")
+USE_CREDENTIALS = get_env_variable("USE_CREDENTIALS")
+VALIDATE_CERTS = get_env_variable("VALIDATE_CERTS")
 
-OTP_SECRET = os.getenv("OTP_SECRET")
-OTP_LIFESPAN_SEC = os.getenv("OTP_LIFESPAN_SEC")
+OTP_SECRET = get_env_variable("OTP_SECRET")
+OTP_LIFESPAN_SEC = get_env_variable("OTP_LIFESPAN_SEC")
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-OPENAI_ORG_ID = os.getenv("OPENAI_ORG_ID")
-OPENAI_MODEL_NAME = os.getenv("OPENAI_MODEL_NAME")
+OPENAI_API_KEY = get_env_variable("OPENAI_API_KEY")
+OPENAI_ORG_ID = get_env_variable("OPENAI_ORG_ID")
+OPENAI_MODEL_NAME = get_env_variable("OPENAI_MODEL_NAME")
 
-VERY_EASY_DIFF_THRESHOLD = os.getenv("VERY_EASY_DIFF_THRESHOLD")
-EASY_DIFF_THRESHOLD = os.getenv("EASY_DIFF_THRESHOLD")
-MEDIUM_DIFF_THRESHOLD = os.getenv("MEDIUM_DIFF_THRESHOLD")
-HARD_DIFF_THRESHOLD = os.getenv("HARD_DIFF_THRESHOLD")
+VERY_EASY_DIFF_THRESHOLD = get_env_variable("VERY_EASY_DIFF_THRESHOLD")
+EASY_DIFF_THRESHOLD = get_env_variable("EASY_DIFF_THRESHOLD")
+MEDIUM_DIFF_THRESHOLD = get_env_variable("MEDIUM_DIFF_THRESHOLD")
+HARD_DIFF_THRESHOLD = get_env_variable("HARD_DIFF_THRESHOLD")
