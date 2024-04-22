faa5a112a458c90f3f28e583e9907c406abcb7b7
Annava Wisha Sikoko <annava.wisha@ui.ac.id>
2024-04-23 03:08:29 +0700

    refactor: logic into upload service

--- src/controllers/upload.py
+++ src/controllers/upload.py
@@ -3,19 +3,17 @@ import mimetypes
 import boto3
 from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
 from src.models.users import User
-from src.services.upload import sha
 from src.services.users import get_current_user
+from src.services.upload import UploadService, sha
 
 from src.utils.settings import AWS_ACCESS_KEY_ID, AWS_BUCKET_NAME, AWS_SECRET_ACCESS_KEY
 from starlette.status import (
     HTTP_200_OK,
-    HTTP_204_NO_CONTENT,
     HTTP_400_BAD_REQUEST,
     HTTP_401_UNAUTHORIZED,
     HTTP_404_NOT_FOUND,
     HTTP_500_INTERNAL_SERVER_ERROR,
 )
-from botocore.exceptions import ClientError
 
 s3_client = boto3.client(
     "s3",
@@ -37,8 +35,7 @@ async def upload_file(
 ):
     user_id = user.id
     try:
-        allowed_types = ["audio/mp4", "audio/mp4a-latm",
-                         "audio/x-m4a", "audio/mpeg"]
+        allowed_types = ["audio/mp4", "audio/mp4a-latm", "audio/x-m4a", "audio/mpeg"]
         file_type, _ = mimetypes.guess_type(file.filename)
 
         # NOTE delete
@@ -65,35 +62,24 @@ async def upload_file(
 
 @upload_router.delete("/delete/{filename}")
 async def delete_audio(filename: str, user: User = Depends(get_current_user)):
-    user_id = str(user.id)
-    uuid_from_filename = filename.split('_')[0]
-
-    if uuid_from_filename != user_id:
+    service =  UploadService(AWS_BUCKET_NAME)
+    if not service.check_user_ownership(filename, str(user.id)):
         raise HTTPException(
             status_code=HTTP_401_UNAUTHORIZED,
             detail="Unauthorized to delete this file"
         )
 
     try:
-        s3_client.head_object(Bucket=AWS_BUCKET_NAME, Key=filename)
-        s3_client.delete_object(Bucket=AWS_BUCKET_NAME, Key=filename)
-        
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'NoSuchKey' or e.response['Error']['Code'] == '404':
-            raise HTTPException(
-                status_code=HTTP_404_NOT_FOUND,
-                detail="File not found"
-            )
-        else:
-            raise HTTPException(
-                status_code=HTTP_500_INTERNAL_SERVER_ERROR,
-                detail=f"AWS S3 error: {str(e)}"
-            )
-            
-    except Exception as e:
+        service.delete_file(filename)
+    except ValueError as e:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
+    except RuntimeError as e:
         raise HTTPException(
             status_code=HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Unexpected server error: {str(e)}"
+            detail=str(e)
         )
 
     return {"status_code": 200, "message": "Successfully deleted the audio file"}
