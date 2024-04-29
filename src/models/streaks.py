62e0c37a1b6db2692049de06d2aa2a4359533e5d
jeremyalv <jeremyalvax@gmail.com>
2024-04-30 00:23:59 +0700

    feat: add streaks Data Model

---
+++ src/models/streaks.py
@@ -0,0 +1,54 @@
+import uuid
+from pytz import timezone
+from datetime import datetime
+
+from src.utils.db import Base
+from src.models.base import DBBase
+from src.models.users import User
+from src.schemas.transcription import TranscriptionSchema, TranscriptionChunksSchema
+
+from sqlalchemy import (
+    Column,
+    UUID,
+    TIMESTAMP,
+    Integer,
+    String,
+    Float,
+    Boolean,
+    ForeignKey,
+)
+
+from sqlalchemy.dialects.postgresql import ARRAY
+
+from sqlalchemy.sql import func
+
+from sqlalchemy.orm import (
+  Mapped,
+  mapped_column,
+  DeclarativeBase,
+  relationship,
+)
+
+from utils.time import get_datetime_now_jkt
+
+class Streaks(Base):
+  __tablename__ = "streaks"
+
+  id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4(), unique=True)
+  owner_id = Column(UUID, ForeignKey(User.id), nullable=False)
+  
+  created_at = Column(TIMESTAMP(timezone=True), default=get_datetime_now_jkt, nullable=False)
+  updated_at = Column(TIMESTAMP(timezone=True), default=get_datetime_now_jkt, onupdate=get_datetime_now_jkt, nullable=False)
+  is_deleted = Column(Boolean, default=False, nullable=False)
+
+  length_days = Column(Integer, default=0, nullable=False)
+  is_active = Column(Boolean, default=True, nullable=False)
+
+  started_at = Column(TIMESTAMP(timezone=True), default=get_datetime_now_jkt, nullable=False)
+  ended_at = Column(TIMESTAMP(timezone=True), nullable=True)
+
+
+
+
+
+
--- src/models/transcription.py
+++ src/models/transcription.py
@@ -37,7 +37,7 @@ class Transcription(Base):
   id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
   created_at = Column(TIMESTAMP(timezone=True), default=get_datetime_now_jkt, nullable=False)
   updated_at = Column(TIMESTAMP(timezone=True), default=get_datetime_now_jkt, onupdate=get_datetime_now_jkt, nullable=False)
-  is_deleted = Column(Boolean, default=False)
+  is_deleted = Column(Boolean, default=False, nullable=False)
 
   ## Foreign Relationships
   # Foreign key to User
