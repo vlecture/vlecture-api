2a613910735f0d8f9d15938b6d3d61c2bf90b084
jeremyalv <jeremyalvax@gmail.com>
2024-04-30 00:29:08 +0700

    feat: create streak schema

---
+++ src/schemas/streaks.py
@@ -0,0 +1,31 @@
+from uuid import UUID
+from datetime import datetime
+
+from typing import (
+  Annotated,
+  Optional,
+  List,
+  Any
+)
+from pydantic import (
+  BaseModel, 
+  BeforeValidator,
+  Field,
+  ConfigDict,
+)
+
+from src.schemas.base import DBBaseModel
+
+# OBJECT SCHEMA
+class StreaksSchema(DBBaseModel, BaseModel):
+  """
+  Schema for Flashcard Streaks object
+  """
+
+  owner_id: UUID
+  length_days: int
+  is_active: bool
+  
+  started_at: datetime
+  ended_at: Optional[datetime]
+
